4d57b5c86a18ed7d7adc482724c004dc635fc3f2
Vinicios Neves <vinicios_neves@hotmail.com>
2023-11-17 15:09:30 +0000

    video 2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
