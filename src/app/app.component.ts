9c9c51761f27d92b13636fec1dbb1c6c94ef8cf4
Vinicios Neves <vinicios_neves@hotmail.com>
2023-11-21 13:29:16 +0000

    aula 3: mão na massa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,19 @@ export class AppComponent {
   contatos: Contato[] = agenda
 
   filtraContatoPorletra(letra: string): Contato[] {
-    return this.contatos.filter(contato => contato.nome.toLowerCase().startsWith(letra))
+    return this.contatos.filter(contato => {
+      // Normaliza o nome do contato para remover acentos. O método 'normalize'
+      // com o argumento "NFD" (Decomposição Normalizada da Forma Canônica) separa
+      // os acentos das letras. O 'replace' então remove os acentos (marcas diacríticas).
+      const nomeNormalizado = contato.nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+      // Converte o nome normalizado para minúsculas para garantir uma comparação
+      // insensível a maiúsculas/minúsculas.
+      const nomeMinusculo = nomeNormalizado.toLowerCase();
+
+      // Verifica se o nome do contato começa com a letra especificada.
+      // Retorna 'true' se o nome começar com a letra, 'false' caso contrário.
+      return nomeMinusculo.startsWith(letra);
+    });
   }
 }
